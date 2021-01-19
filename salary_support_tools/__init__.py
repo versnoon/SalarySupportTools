2671c84257e6fc5e17623ea7707cf76c5a1b07bc
tongt <versnoon@gmail.com>
2021-01-19 13:19:15 +0800

    项目初始化

---
+++ salary_support_tools/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py
+@Time    :   2021/01/19 10:26:59
+@Author  :   Tong tan 
+@Version :   1.0
+@Contact :   tongtan@gmail.com
+'''
+
+
+if __name__ == "__main__":
+    print("run in main")
+else:
+    print("run in sub module")
---
+++ salary_support_tools/runner.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   runner.py
+@Time    :   2021/01/19 10:29:33
+@Author  :   Tong tan 
+@Version :   1.0
+@Contact :   tongtan@gmail.com
+'''
+
+def start():
+   return "func runner"
+
+
+if __name__ == "__main__":
+    print("running")
