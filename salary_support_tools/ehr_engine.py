1e5d6d0230417bca34aa7b93691ac107a03c8f74
tongt <versnoon@gmail.com>
2021-01-22 14:11:14 +0800

    完善审核表相关信息

--- salary_support_tools/ehr_engine.py
+++ salary_support_tools/ehr_engine.py
@@ -406,7 +406,7 @@ class SalaryJjInfo(object):
     奖金信息
     """
 
-    def __init__(self, code="", name="", departfullinfo="", distributionMark="", ysjse=0, bonusTow=0, gtsyj=0, pay=0, jjhj=0, totalPayable=0, jsjseptsl=0, jbjj=0, gts=0, bonusOne=0, bonusThree=0, yseyhsl=0, yse=0, gstz=0):
+    def __init__(self, code="", name="", departfullinfo="", distributionMark="", ysjse=0, bonusTow=0, gtsyj=0, pay=0, jjhj=0, totalPayable=0, jsjseptsl=0, jbjj=0, gts=0, bonusOne=0, bonusThree=0, yseyhsl=0, yse=0, gstz=0, gcjj=0, jssc=0, nddxj=0, jsjj=0, qt=0, gsxyj=0):
         self._code = code
         self._name = name
         self._departfullinfo = departfullinfo
@@ -425,6 +425,12 @@ class SalaryJjInfo(object):
         self._yse = yse
         self._totalPayable = totalPayable
         self._gstz = gstz
+        self._gcjj = gcjj
+        self._jssc = jssc
+        self._nddxj = nddxj
+        self._jsjj = jsjj
+        self._qt = qt
+        self._gsxyj = gsxyj
 
     def __str__(self):
         return '员工奖金信息: 机构 {} - 工号 {} - 姓名 {} - 应发 {} - 实发 {}'.format(self._departfullinfo, self._code, self._name, self._totalPayable, self._pay)
@@ -449,8 +455,12 @@ class SalaryJjInfo(object):
         columns["_bonusThree"] = "单项奖3"
         columns["_yseyhsl"] = "应税额"
         columns["_totalPayable"] = "应发"
-        columns["gstz"] = "个税调整"
-
+        columns["_gstz"] = "个税调整"
+        columns["_gcjj"] = "工程津贴"
+        columns["_jssc"] = "技术输出"
+        columns["_qt"] = "争取国家政策奖"
+        columns["_nddx"] = "年底兑现奖"
+        columns["_jsjj"] = "计税奖金"
         return columns
 
     def get_exl_tpl_folder_path(self):
@@ -619,7 +629,7 @@ class Auditor(object):
     审核表业务模型
     """
 
-    def __init__(self, period, salaryScope, code="", name="", gwgz=0, blgz=0, nggz=0, fzgz=0, shbz=0, jbjj=0, bankno1="", bankinfo1=""):
+    def __init__(self, period, salaryScope, code="", name="", gwgz=0, blgz=0, nggz=0, fzgz=0, shbz=0, khgz=0, gzbt=0, qtgz=0, ntjbgz=0, ntzz=0, ntglgz=0, djsj=0, ybjt=0, jsjt=0, yzdnjt=0, ksjt=0, xjjt=0, zwjt=0, wyjt=0, bzzjt=0, kjjt=0, nsjt=0, totaljbf=0, totalqq=0, gjj=0, yl=0, sy=0, yil=0, nj=0, totalsdj=0, sljj=0, cwkk=0, cwbt=0, wybt=0, bjf=0, db=0, txf=0, gwf=0, hm=0, wcf=0, scjt=0, zwbt=0, kyxm=0, jsgg=0, fgzjtbf=0, yznx=0, totalpayalbe=0, totalpay=0, jyjf=0, gcjt=0, jssc=0, qt=0, jbjj=0, gsxyj=0, nddxj=0, nddxjpay=0, nddxjtex=0, jsjj=0, totaljj=0, gzpay=0, jjpay=0, bankno1="", bankinfo1="", bankno2="", bankinfo2="", ygz=""):
         self.period = period  # 期间
         self.salaryScope = salaryScope  # 工资范围
         self._code = code  # 职工编码
@@ -630,17 +640,109 @@ class Auditor(object):
         self._nggz = nggz  # 年功工资
         self._fzgz = fzgz  # 辅助工资
         self._shbz = shbz  # 生活补助
+        self._khgz = khgz  # 考核工资
+        self._gzbt = gzbt  # 工资补退
+        self._qtgz = qtgz  # 其他工资
+        self._ntjbgz = ntjbgz  # 内退基本工资
+        self._ntzz = ntzz  # 内退增资
+        self._ntglgz = ntglgz  # 内退工龄工资
+        self._djsj = djsj  # 代缴三金
+        self._wjbt = 0  # 物价补贴
+        self._ybjt = ybjt  # 夜班津贴
+        self._jsjt = jsjt  # 技师津贴
+        self._yzdnjt = yzdnjt  # 一转多能津贴(纪检 计生 信访)
+        self._ksjt = ksjt  # 矿山津贴
+        self._xjjt = xjjt  # 下井津贴
+        self._zwjt = zwjt  # 驻外津贴 （原教护龄津贴）
+        self._wyjt = wyjt  # 外语津贴
+        self._bzzjt = bzzjt  # 班组长津贴
+        self._kjjt = kjjt  # 科技津贴
+        self._nsjt = nsjt  # 能手津贴
+        self._totaljj = totaljj  # 奖金合计
+        self._totaljbf = totaljbf  # 加班费合计
+        self._totalqq = totalqq  # 缺勤扣款合计
+        self._yznx = yznx  # 预支年薪
+        self._zygz = 0  # 执业工资
+
+        self._gjj = gjj  # 公积金个人
+        self._yl = yl  # 养老保险个人
+        self._sy = sy  # 失业保险个人
+        self.yil = yil  # 医疗保险个人
+        self._nj = nj  # 年金保险个人
+
+        # 其他扣缴
+        self._sljj = sljj  # 水利基金
+        self._cwkk = cwkk  # 财务扣款
+
+        self._df = 0  # 电费
+        self._fz = 0  # 房租
+        self._dsf = 0  # 电视费
+        self._qjf = 0  # 清洁费
+        self._ccf = 0  # 乘车费
+        self._cwbt = cwbt  # 财务补退
+
+        # 非工资信息
+        self._wybt = wybt  # 物业补贴
+        self._bjf = bjf  # 保健费
+        self._db = db  # 独补
+        self._txf = txf  # 通讯费
+        self._gwf = gwf  # 高温津贴
+        self._hm = hm  # 回民
+        self._jj = 0  # 纪检津贴
+        self._js = 0  # 计生津贴
+        self._wcf = wcf  # 误餐费
+        self._ksryj = 0  # 矿山荣誉金
+        self._xf = 0  # 信访津贴
+        self._scjt = scjt  # 伤残津贴
+        self._zwbt = zwbt  # 职务补贴
+        self._kyxm = kyxm  # 科研项目津贴
+        self._jsgg = jsgg  # 技术攻关津贴
+        self._fgzjtbf = fgzjtbf  # 非工资津贴补发
 
         # 奖金信息
         self._jbjj = jbjj  # 基本奖金
+        self._gsxyj = gsxyj  # 公司效益奖
+        self._gsxyjpay = 0  # 公司效益奖上卡
+        self._gsxyjtex = 0  # 公司效益奖所得税
+        self._nddxj = nddxj  # 年底兑现奖
+        self._nddxjpay = 0  # 年底兑现将实发
+        self._nddxjtex = 0  # 年底兑现将所得税
+        self._jsjj = jsjj  # 计税奖金
+        self._gcjt = gcjt  # 工程津贴
+        self._jssc = jssc  # 技术输出
+        self._qt = qt  # 争取国家政策等
         # 账号信息
         self._bankno1 = bankno1  # 银行账号1
         self._bankinfo1 = bankinfo1  # 银行1
+        self._bankno2 = bankno2  # 银行账号1
+        self._bankinfo2 = bankinfo2  # 银行1
+
+        # 汇总
+
+        self._totalsdj = totalsdj  # 所得税
+        self._totalpayalbe = totalpayalbe  # 应发
+        self._totalpay = totalpay  # 实发
+        self._gzpay = gzpay  # 工资上卡
+        self._jjpay = jjpay  # 奖金上卡
+        self._nddxjpay = 0  # 年终奖上卡
+        self._sfhd = 0  # 实发核对
+
+        # 代发项目
+
+        self._jyjf = jyjf  # 教育经费
+        self._gcjt = gcjt  # 工程津贴
+        self._jssc = jssc  # 技术输出
+        self._qt = qt  # 其他
+
+        self._ygz = ygz
+        self._ygzz = ""
 
     def to_auditor(self, personinfo: PersonInfo, gzinfo: SalaryGzInfo, jjinfo: SalaryJjInfo, bankinfo: SalaryBankInfo):
         if personinfo is not None:
             self._code = personinfo._code
             self._name = personinfo._name
+            self._ygz = personinfo._personType
+            self._ygzz = personinfo._jobStatus
         else:
             if jjinfo is not None:
                 self._code = jjinfo._code
@@ -653,14 +755,198 @@ class Auditor(object):
             self._nggz = gzinfo._glgz
             self._fzgz = gzinfo._qtblgz
             self._shbz = gzinfo._shbt_jt
+            self._khgz = gzinfo._khfd
+            self._gzbt = gzinfo._gztz
+            self._qtgz = gzinfo._shf
+            self._ntjbgz = gzinfo._gdgz
+            self._ntzz = gzinfo._dtxgz
+            self._ntglgz = gzinfo._glgz
+            self._djsj = gzinfo._shfbc_jt
+            self._wjbt = 0
+            self._ybjt = gzinfo._zyb_jt
+            self._jsjt = gzinfo._jn_jt
+            self._yzdnjt = gzinfo._jggz_jt
+            self._ksjt = 0
+            self._xjjt = 0
+            self._zwjt = gzinfo._zw_jt
+            self._wyjt = gzinfo._xl_jt
+            self._bzzjt = 0
+            self._kjjt = gzinfo._kjyx_jt
+            self._nsjt = gzinfo._czns_jt
+            self._totaljbf = gzinfo._total_jbgz
+            self._totalqq = gzinfo._total_kk
+
+            self._gjj = 0 - gzinfo._gjj_bx
+            self._yl = 0 - gzinfo._yl_bx
+            self._sy = 0 - gzinfo._sy_bx
+            self._yil = 0 - gzinfo._yil_bx
+            self._nj = 0 - gzinfo._nj_bx
+
+            if gzinfo._gts != "":
+                self._totalsdj = 0 - gzinfo._gts  # 工资所得税
+
+            if gzinfo._qtdkk != "":
+                self._sljj = 0 - gzinfo._qtdkk  # 水利基金
+            if gzinfo._qtkk != "":
+                self._cwkk = 0 - gzinfo._qtkk  # 其他扣款
+
+            self._wyjt = gzinfo._sdqnwy_jt  # 物业补贴
+            self._bjf = gzinfo._cq_jt
+            self._db = gzinfo._dsznf
+            self._txf = gzinfo._tx_jt
+            self._gwf = gzinfo._gw_jt
+            self._hm = gzinfo._mz_jt
+            self._wcf = gzinfo._wc_jt
+            self._scjt = gzinfo._gs_jt
+            self._zwbt = gzinfo._gongw_jt
+            self._kyxm = gzinfo._tsgx_jt
+            self._jsgg = gzinfo._js_jt
+            self._fgzjtbf = gzinfo._gxbt_jt
+            self._yznx = gzinfo._ygdxz + gzinfo._jxgz
+
+            self._totalpayalbe = gzinfo._totalPayable   # 工资应发
+            self._totalpay = gzinfo._pay  # 工资实发
+            self._gzpay = gzinfo._pay  # 工资实发
+
+            self._jyjf = gzinfo._jkdjf
 
         if jjinfo is not None:
             self._jbjj = jjinfo._jbjj
 
+            self._gcjt = jjinfo._gcjj
+            self._jssc = jjinfo._jssc
+            self._qt = jjinfo._qt
+            self._gsxyj = jjinfo._gsxyj
+            self._nddxj = jjinfo._nddxj
+            self._jsjj = jjinfo._jsjj
+
+            self._totaljj = jjinfo._totalPayable
+
+            self._totalsdj = self._totalsdj + 0 - jjinfo._gts  # 合并入奖金所得税
+
+            self._totalpayalbe = self._totalpayalbe + jjinfo._totalPayable  # 合并奖金应发
+            self._totalpay = self._totalpay + jjinfo._totalPayable  # 合并奖金实发
+            self._jjpay = jjinfo._pay  # 奖金实发
+
         if bankinfo is not None:
             self._bankno1 = bankinfo['gz']._bankNo
             self._bankinfo1 = bankinfo['gz']._financialInstitution
+            if 'jj' in bankinfo:
+                self._bankno2 = bankinfo['jj']._bankNo
+                self._bankinfo2 = bankinfo['jj']._financialInstitution
+
+    def columnsDef(self):
+        columns = dict()
+        columns["period"] = "发薪期间"
+        columns["salaryScope"] = "工资范围"
+        columns["_code"] = "员工编号"
+        columns["_name"] = "员工姓名"
+        columns["_gwgz"] = "岗位工资"
+        columns["_blgz"] = "保留工资"
+        columns["_nggz"] = "年功工资"
+        columns["_fzgz"] = "辅助工资"
+        columns["_shbt"] = "生活补贴"
+        columns["_khgz"] = "考核工资"
+        columns["_gzbt"] = "工资补退"
+        columns["_qtgz"] = "其他工资"
+        columns["_ntjbgz"] = "内退基本工资"
+        columns["_ntzz"] = "内退增资"
+        columns["_ntglgz"] = "内退工龄工资"
+        columns["_djsj"] = "代缴三金"
+        columns["_wjbt"] = "物价补贴"
+        columns["_ybjt"] = "夜班津贴"
+        columns["_jsjt"] = "技师津贴"
+        columns["_yzdnjt"] = "一专多能、计生、纪检、信访津贴"
+        columns["_ksjt"] = "矿山津贴"
+        columns["_xjjt"] = "下井津贴"
+        columns["_zwjt"] = "驻外津贴"
+        columns["_wyjt"] = "外语津贴"
+        columns["_bzzjt"] = "班组长津贴"
+        columns["_kjjt"] = "科技津贴"
+        columns["_nsjt"] = "能手津贴"
+        columns["_totaljj"] = "奖金合计"
+        columns["_totaljbf"] = "加班费合计"
+        columns["_totalqq"] = "缺勤扣款合计"
+        columns["_gjj"] = "公积金"
+        columns["_yl"] = "养老保险"
+        columns["_sy"] = "失业保险"
+        columns["_yil"] = "医疗保险"
+        columns["_nj"] = "年金"
+        columns["_totalsdj"] = "工资税收"
+        columns["_sljj"] = "水利基金"
+        columns["_df"] = "电费"
+        columns["_fz"] = "房租"
+        columns["_dsf"] = "电视费"
+        columns["_qjf"] = "清洁费"
+        columns["_ccf"] = "乘车费"
+        columns["_cwbt"] = "财务补退"
+        columns["_wybt"] = "物业补贴"
+        columns["_bjf"] = "保健费"
+        columns["_db"] = "独补"
+        columns["_txf"] = "通讯费"
+        columns["_gwf"] = "高温津贴"
+        columns["_hm"] = "回民"
+        columns["_jj"] = "纪检津贴"
+        columns["_js"] = "计生津贴"
+        columns["_wc"] = "误餐补贴"
+        columns["_ksryj"] = "矿山荣誉金"
+        columns["_xf"] = "信访津贴"
+        columns["_scjt"] = "伤残津贴"
+        columns["_zwbt"] = "职务补贴"
+        columns["_kyxm"] = "科研项目津贴"
+        columns["_jsgg"] = "技术攻关津贴"
+        columns["_fgzjtbf"] = "非工资性津贴补发"
+
+        columns["_totalpayalbe"] = "应发合计"
+        columns["_totalpay"] = "实发合计"
+        columns["_jyjf"] = "教育经费"
+        columns["_gcjj"] = "工程津贴"
+        columns["_jssc"] = "技术输出"
+        columns["_qt"] = "其他"
+        columns["_gsxyj"] = "公司效益奖"
+        columns["_gsxyjpay"] = "公司效益奖上卡"
+        columns["_gsxyjtex"] = "公司效益奖所得税"
+        columns["_nddxj"] = "年底兑现奖"
+        columns["_nddxjpay"] = "年底兑现奖实发"
+        columns["_nddxjtex"] = "年底兑现奖所得税"
+        columns["_jsjj"] = "计税奖金"
+        columns["_yznx"] = "预支年薪"
+        columns["_zygz"] = "执业工资"
+        columns["_bankno1"] = "银行卡1"
+        columns["_bankinfo1"] = "银行1"
+        columns["_bankno2"] = "银行卡2"
+        columns["_bankinfo2"] = "银行2"
+        columns["_gzpay"] = "上卡工资"
+        columns["_nddxjpay"] = "上卡年终奖"
+        columns["_jjpay"] = "上卡基本奖"
+        columns["_sfhd"] = "实发核对"
+        columns["_ygz"] = "员工组"
+        columns["_ygzz"] = "员工子组"
+
+        return columns
 
     def __str__(self):
-        return '审批表信息: 发薪日期 {} - 工资范围 {} - 职工编码 {} - 姓名 {} - 岗位工资 {} - 基本奖金 {} - 工资卡号 {} - 工资卡金融机构 {}'.format(
-            self.period, self.salaryScope, self._code, self._name, self._gwgz, self._jbjj, self._bankno1, self._bankinfo1)
+        return '审批表信息: 发薪日期 {} - 工资范围 {} - 职工编码 {} - 姓名 {} - 人员类型 {} - 在职状态 {} - 应发合计 {} - 奖金合计 {} - 公积金 {} - 养老保险 {} - 失业保险 {} - 医疗保险 {} - 年金 {} - 所得税 {} - 实发合计 {} - 工资卡号 {} - 工资卡金融机构 {} - 奖金卡号 {} - 奖金卡金融机构 {}'.format(
+            self.period, self.salaryScope, self._code, self._name, self._ygz, self._ygzz, self._totalpayalbe, self._totaljj, self._gjj, self._yl, self._sy, self._yil, self._nj, self._totalsdj, self._totalpay, self._bankno1, self._bankinfo1, self._bankno2, self._bankinfo2)
+
+
+class TexOperator(object):
+    """
+    税单相关表
+    """
+
+    def __init__(self, datas,):
+        self._datas = datas
+
+
+class TexInfo(object):
+    """
+    所得税相关信息定义
+    """
+
+    def __init__(self, code, name, certificateType, idno, totalpayable):
+        self._code = code
+        self._name = name
+        self._certificateType = certificateType
+        self._idno = idno
+        self._totalpayable = totalpayable
